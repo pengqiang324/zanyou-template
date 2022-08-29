a2b6acd76731743769458f021238b6601bbe6eae
pengqiang <pengqiang@51qub.com>
2022-08-29 10:59:04 +0800

    更新路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { defineAsyncComponent } from 'vue'
+import config, { SINGLE } from "@/config/defaultSettings";
 
+// 开发环境热更新异步加载组件
+const asyncComponent = (Component) => {
+  if (process.env.NODE_ENV === 'development') {
+    return defineAsyncComponent(Component)
+  } else {
+    return Component
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'HomeView',
-    component: defineAsyncComponent(() => import(/* webpackChunkName: "Home" */ '../views/Home.vue')),
+    component: asyncComponent(() => import(/* webpackChunkName: "Home" */ '../views/Home.vue')),
     meta: {
       keepAlive: true
     }
@@ -17,21 +26,51 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: defineAsyncComponent(() => import(/* webpackChunkName: "about" */ '../views/About.vue')),
+    component: asyncComponent(() => import(/* webpackChunkName: "about" */ '../views/About.vue')),
     meta: {
       keepAlive: true
     }
   },{
     path: '/:pathMatch(.*)',
     name: '404',
-    component: defineAsyncComponent(() => import(/* webpackChunkName: "404" */ '../views/error-page/404.vue'))
+    component: asyncComponent(() => import(/* webpackChunkName: "404" */ '../views/error-page/404.vue'))
   }
 ]
 
+// 生产环境单例模式路由
+const productioinSingleRouter = [
+  {
+    path: "/",
+    name: "404",
+    redirect: "/404"
+  },
+  {
+    path: "/404",
+    component: asyncComponent(() => import("@/views/error-page/404")),
+    hidden: true
+  },
+  {
+    path: "/:pathMatch(.*)",
+    redirect: "/404",
+    hidden: true
+  }
+];
 
+// 根据访问模型与环境过滤路由
+const filterRouter = () => {
+  if (config.developerModel === SINGLE) {
+    if (process.env.NODE_ENV === "development") {
+      return routes;
+    } else {
+      return productioinSingleRouter;
+    }
+  } else {
+    return routes;
+  }
+};
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: routes
-})
+  routes: filterRouter()
+});
 
 export default router
